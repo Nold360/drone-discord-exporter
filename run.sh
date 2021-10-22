9bcfe1b83e62dfc87e56935d850a78f39d238274
nold <nold@gnu.one>
2021-10-22 15:46:45 +0200

    test

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+IFS=$'\n'
 
-for cid in $(grep -v '^#' channels.list); do
-  /app/DiscordChatExporter.Cli export -f Json --media -c ${cid}
+for cid in $(grep -v -E '^#|^$' channels.list); do
+  /app/DiscordChatExporter.Cli export -f Json --media -c ${cid} --after 901051269223968778 -o /cache
+done
+
+for file in $(find /cache/*) ; do
+  mv -v "${file}" /cache/$(basename "$file" | sed -e 's/[^A-Za-z0-9._-]/_/g')
 done
 
 exit 0
